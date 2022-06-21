4260f28efb5c7611e100335cec07e50bed87e04f
GytisKlnt <gytis.kalanta@gmail.com>
2022-06-21 19:51:19 +0300

    added login post

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
 const bcrypt = require('bcrypt');
+const jsonwebtoken = require('jsonwebtoken');
 
-const { registrationSchema } = require('../../middleware/valSchemas');
-const { mysqlConfig } = require('../../config');
+const { registrationSchema, loginSchema } = require('../../middleware/valSchemas');
+const { mysqlConfig, jwtSecret } = require('../../config');
 const validation = require('../../middleware/validation');
 
 const router = express.Router();
@@ -25,6 +26,33 @@ router.post('/register', validation(registrationSchema), async (req, res) => {
     return res.send({
       msg: 'Succesfully created account',
       userId: data.insertId,
+    });
+  } catch (err) {
+    return res.status(500).send({ msg: 'An issue was found. Please try again later.' });
+  }
+});
+
+router.post('/login', validation(loginSchema), async (req, res) => {
+  try {
+    const con = await mysql.createConnection(mysqlConfig);
+    const [data] = await con.execute(`
+        SELECT * FROM users WHERE email = ${mysql.escape(req.body.email)} LIMIT 1
+        `);
+    await con.end();
+
+    if (!data.length === 0) {
+      return res.status(400).send({ msg: 'Incorrect email or password' });
+    }
+
+    if (!bcrypt.compareSync(req.body.password, data[0].password)) {
+      return res.status(400).send({ msg: 'Incorrect email or password' });
+    }
+
+    const token = jsonwebtoken.sign({ accountId: data[0].id }, jwtSecret);
+
+    return res.send({
+      msg: 'Succesfully logged in',
+      token,
     });
   } catch (err) {
     return res.status(500).send({ msg: 'An issue was found. Please try again later.' });
